0b51f6f856a8367bf3fc9871dcd566978ee25784
System Administrator <root@Mourads-Air.lan>
2020-12-19 15:42:04 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! this is my custom project'));
+app.get('/', (req, res) => res.send('Hello World!this is a full aws pipeline test from Mourad Oulmas'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
